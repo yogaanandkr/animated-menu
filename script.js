5ccab0f9cb3a545deb0f20d139b34e5ef998e139
Yoga Anand K.R <yokeshanand762001@gmail.com>
2022-04-18 14:55:36 +0530

    'nav'

---
+++ script.js
@@ -0,0 +1,8 @@
+const list = document.querySelectorAll(".active");
+const icons = document.querySelectorAll(".icon");
+const text = document.querySelectorAll(".text");
+const btn = document.getElementById("btn");
+
+icon.addEventListener("mouseover", function () {
+  document.body.style.backgroundColor = "red";
+});
